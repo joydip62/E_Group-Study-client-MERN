b0b022b363f8f4afb945d2dc9f7fe90a71539f83
Stefany Cardozo <joydipbanik6@gmail.com>
2023-11-05 17:45:31 +0530

    firebase authtication with social login

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import routes from './routes'
+import AuthProvider from './provider/AuthProvider'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={routes}></RouterProvider>
-  </React.StrictMode>,
-)
+    <AuthProvider>
+      <RouterProvider router={routes}></RouterProvider>
+    </AuthProvider>
+  </React.StrictMode>
+);
