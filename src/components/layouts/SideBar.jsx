fd1f74c350b6d1f0e01d0b352504122f991f7110
Stefany Cardozo <joydipbanik6@gmail.com>
2023-11-05 18:26:15 +0530

    added toggle theme

--- src/components/layouts/SideBar.jsx
+++ src/components/layouts/SideBar.jsx
@@ -1,10 +1,30 @@
 import { NavLink } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+import Theme from "../../shared/Theme";
 
 const SideBar = () => {
+  const { user, logout } = useAuth();
+  
     return (
       <div className="flex flex-col gap-5">
-        
-        <h2 className="">user Name</h2>
+        {user?.email ? (
+          <div>
+            <img
+              src={
+                user.photoURL
+                  ? user.photoURL
+                  : "https://cdn-icons-png.flaticon.com/512/6596/6596121.png"
+              }
+              className="text-center mx-auto"
+            />
+            <h6 className="text-xl font-bold text-center">
+              {user?.displayName ? user?.displayName : "User"}
+            </h6>
+          </div>
+        ) : (
+          <div></div>
+        )}
+
         <NavLink
           to="/"
           className={({ isActive }) =>
@@ -23,27 +43,65 @@ const SideBar = () => {
           Assignments
         </NavLink>
 
-        <NavLink
-          to="/login"
-          className={({ isActive }) =>
-            isActive ? "btn btn-neutral" : "btn btn-ghost"
-          }
-        >
-          Login
-        </NavLink>
-        <NavLink
-          to="/register"
-          className={({ isActive }) =>
-            isActive ? "btn btn-neutral" : "btn btn-ghost"
-          }
-        >
-          Register
-        </NavLink>
-        
-        <NavLink>
-          Logout
-        </NavLink>
+        {!user?.email ? (
+          <>
+            <NavLink
+              to="/login"
+              className={({ isActive }) =>
+                isActive ? "btn btn-neutral" : "btn btn-ghost"
+              }
+            >
+              Login
+            </NavLink>
+            <NavLink
+              to="/register"
+              className={({ isActive }) =>
+                isActive ? "btn btn-neutral" : "btn btn-ghost"
+              }
+            >
+              Register
+            </NavLink>
+          </>
+        ) : (
+          <>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? "btn btn-sm btn-neutral" : "btn btn-sm btn-ghost"
+              }
+            >
+              Create assignments
+            </NavLink>
 
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? "btn btn-sm btn-neutral" : "btn btn-sm btn-ghost"
+              }
+            >
+              My assignments
+            </NavLink>
+
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? "btn btn-sm btn-neutral" : "btn btn-sm btn-ghost"
+              }
+            >
+              Submitted assignments links
+            </NavLink>
+            <a
+              onClick={logout}
+              className="text-lg text-center mx-auto font-semibold"
+            >
+              LOGOUT
+            </a>
+          </>
+        )}
+
+        <div className="text-center mx-auto">
+          <Theme />
+        </div>
       </div>
     );
 };
