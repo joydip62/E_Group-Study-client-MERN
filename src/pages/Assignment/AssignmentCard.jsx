077f44e2f66f33530185cd577c59d368f889a42f
Stefany Cardozo <joydipbanik6@gmail.com>
2023-11-06 20:33:36 +0530

    assignment delete from database. the assignment delete only those user who created the assignment functionality added

---
+++ src/pages/Assignment/AssignmentCard.jsx
@@ -0,0 +1,57 @@
+/* eslint-disable react/prop-types */
+
+
+const AssignmentCard = ({ assignment, handleDeleteAssignment }) => {
+
+  const {
+    _id,
+    title,
+    marks,
+    difficultyLevel,
+    description,
+    dueDate,
+    image,
+    assignmentCreator,
+  } = assignment;
+
+  return (
+    <div>
+      <div className="card card-compact w-96 bg-base-100 shadow-xl mb-10">
+        <figure>
+          <img src={image} alt={title} />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{title}</h2>
+          <p>{description}</p>
+          <div className="card-actions justify-start">
+            <div className="badge badge-outline">{dueDate}</div>
+            <div className="badge badge-outline">{difficultyLevel}</div>
+          </div>
+          <p className="font-bold">Marks : {marks}</p>
+          <p className="font-bold">User : {assignmentCreator}</p>
+
+          <div className="flex justify-end gap-5">
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Update Assignment</button>
+            </div>
+
+            <div className="card-actions justify-end">
+              <button className="btn btn-info">View Assignment</button>
+            </div>
+
+            <div className="card-actions justify-end">
+              <button
+                onClick={() => handleDeleteAssignment(_id, assignmentCreator)}
+                className="btn btn-error"
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AssignmentCard;
