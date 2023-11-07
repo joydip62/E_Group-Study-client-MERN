7b3f2440db177c430f60ff3cce1ceaaf12c597a5
Stefany Cardozo <joydipbanik6@gmail.com>
2023-11-07 10:27:42 +0530

    handle from validation

--- src/pages/Assignment/CreateAssignment/CreateAssignment.jsx
+++ src/pages/Assignment/CreateAssignment/CreateAssignment.jsx
@@ -6,58 +6,123 @@ import useAxios from "../../../hooks/useAxios";
 import Swal from "sweetalert2";
 
 const CreateAssignment = () => {
+  const [formData, setFormData] = useState({
+    title: "",
+    marks: "",
+    difficultyLevel: "",
+    description: "",
+    dueDate: "",
+    image: "",
+  });
+
+  const [formErrors, setFormErrors] = useState({});
+
+  const { user } = useAuth();
+  const axios = useAxios();
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
-  const [startDate, setStartDate] = useState(new Date());
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
-  const { user } = useAuth();
-  const axios = useAxios();
-  
+  const validateForm = () => {
+    let errors = {};
+
+    if (!formData.title) {
+      errors.title = "Title is required";
+    }
+
+    if (!formData.marks) {
+      errors.marks = "Marks is required";
+    } else if (!/^\d+$/.test(formData.marks)) {
+      errors.marks = "Marks should be a number";
+    }
+
+    if (!formData.difficultyLevel) {
+      errors.difficultyLevel = "Difficulty level is required";
+    }
+
+    if (!formData.description) {
+      errors.description = "Description is required";
+    }
+
+    if (!formData.dueDate) {
+      errors.dueDate = "Due date is required";
+    }
+
+    if (!formData.image) {
+      errors.image = "Image URL is required";
+    } else if (!isValidURL(formData.image)) {
+      errors.image = "Invalid URL";
+    }
+
+    setFormErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
+
+  const isValidURL = (url) => {
+    const urlRegex = /(https?:\/\/.*\.(?:png|jpg))/i;
+    return urlRegex.test(url);
+  };
+
   const handleCreateAssignment = (e) => {
     e.preventDefault();
-    const form = e.target;
-    const title = form.title.value;
-    const marks = form.marks.value;
-    const difficultyLevel = form.difficultyLevel.value;
-    const description = form.description.value;
-    const dueDate = form.dueDate.value;
-    const image = form.image.value;
-    const assignmentCreator = user.email;
+    const isFormValid = validateForm();
 
-    const assignment = {
-      title,
-      marks,
-      difficultyLevel,
-      description,
-      dueDate,
-      image,
-      assignmentCreator,
-    };
+    if (isFormValid) {
+      const { title, marks, difficultyLevel, description, dueDate, image } =
+        formData;
 
- 
+      const assignmentCreator = user.email;
 
-    axios
-      .post("create/assignment", assignment)
-      .then((res) => {
-        console.log(res.data);
-        Swal.fire(
-          "Good job!",
-          "Your Assignment added successfully!",
-          "success"
-        );
-        form.reset();
-      })
-      .catch(function (error) {
-        Swal.fire("Oopsss", error.message, "error");
-      });
+      const assignment = {
+        title,
+        marks,
+        difficultyLevel,
+        description,
+        dueDate,
+        image,
+        assignmentCreator,
+      };
+
+      axios
+        .post("create/assignment", assignment)
+        .then((res) => {
+          console.log(res.data);
+          Swal.fire(
+            "Good job!",
+            "Your Assignment added successfully!",
+            "success"
+          );
+          setFormData({
+            title: "",
+            marks: "",
+            difficultyLevel: "",
+            description: "",
+            dueDate: new Date(),
+            image: "",
+          });
+        })
+        .catch(function (error) {
+          Swal.fire("Oopsss", error.message, "error");
+        });
+    } else {
+      Swal.fire("Oopsss", "Form is not valid", "error");
+    }
   };
   return (
     <div className="w-3/4 m-auto text-center lg:p-24">
       <h2 className="text-3xl mb-8 font-bold">Create Assignment</h2>
       <form onSubmit={handleCreateAssignment}>
         <div className="md:flex space-x-5 mb-8">
+          {/* Title Input */}
           <div className="form-control md:w-1/2">
             <label className="label">
               <span className="label-text">Title</span>
@@ -65,14 +130,20 @@ const CreateAssignment = () => {
             <label className="input-group">
               <input
                 type="text"
-                placeholder="enter assignment title"
+                placeholder="Enter assignment title"
                 className="input input-bordered w-full"
                 name="title"
+                value={formData.title}
+                onChange={handleInputChange}
                 required
               />
             </label>
+            {formErrors.title && (
+              <p className="text-red-500">{formErrors.title}</p>
+            )}
           </div>
 
+          {/* Marks Input */}
           <div className="form-control md:w-1/2">
             <label className="label">
               <span className="label-text">Marks</span>
@@ -80,14 +151,20 @@ const CreateAssignment = () => {
             <label className="input-group">
               <input
                 type="text"
-                placeholder="enter mark"
+                placeholder="Enter mark"
                 className="input input-bordered w-full"
                 name="marks"
+                value={formData.marks}
+                onChange={handleInputChange}
                 required
               />
             </label>
+            {formErrors.marks && (
+              <p className="text-red-500">{formErrors.marks}</p>
+            )}
           </div>
 
+          {/* Difficulty Level Select */}
           <div className="form-control md:w-1/2">
             <label className="label">
               <span className="label-text">Assignment difficulty level</span>
@@ -96,9 +173,11 @@ const CreateAssignment = () => {
               <select
                 className="select select-bordered w-full max-w-xs"
                 name="difficultyLevel"
+                value={formData.difficultyLevel}
+                onChange={handleInputChange}
                 required
               >
-                <option disabled selected value="">
+                <option disabled value="">
                   Select type
                 </option>
                 <option value="Easy">Easy</option>
@@ -106,10 +185,14 @@ const CreateAssignment = () => {
                 <option value="Hard">Hard</option>
               </select>
             </label>
+            {formErrors.difficultyLevel && (
+              <p className="text-red-500">{formErrors.difficultyLevel}</p>
+            )}
           </div>
         </div>
 
         <div className="md:flex space-x-5 mb-8">
+          {/* Description Input */}
           <div className="form-control md:w-3/4">
             <label className="label">
               <span className="label-text">Description</span>
@@ -117,28 +200,39 @@ const CreateAssignment = () => {
             <label className="input-group">
               <input
                 type="text"
-                placeholder="enter description"
+                placeholder="Enter description"
                 className="input input-bordered w-full"
                 name="description"
+                value={formData.description}
+                onChange={handleInputChange}
                 required
               />
             </label>
+            {formErrors.description && (
+              <p className="text-red-500">{formErrors.description}</p>
+            )}
           </div>
+
+          {/* Due Date Datepicker */}
           <div className="form-control md:w-1/4">
             <label className="label">
               <span className="label-text">Due date</span>
             </label>
             <label className="input-group">
               <DatePicker
-                selected={startDate}
-                onChange={(date) => setStartDate(date)}
+                selected={formData.dueDate}
+                onChange={(date) => setFormData({ ...formData, dueDate: date })}
                 name="dueDate"
                 className="border border-gray-300 p-3 rounded-md w-full"
+                required
               />
             </label>
+            {formErrors.dueDate && (
+              <p className="text-red-500">{formErrors.dueDate}</p>
+            )}
           </div>
         </div>
-
+        {/* Image URL Input */}
         <div className="mb-8">
           <div className="form-control w-full">
             <label className="label">
@@ -147,75 +241,26 @@ const CreateAssignment = () => {
             <label className="input-group">
               <input
                 type="text"
-                placeholder="enter thumbnail Image URL"
+                placeholder="Enter thumbnail Image URL"
                 className="input input-bordered w-full"
                 name="image"
+                value={formData.image}
+                onChange={handleInputChange}
                 required
               />
             </label>
+            {formErrors.image && (
+              <p className="text-red-500">{formErrors.image}</p>
+            )}
           </div>
         </div>
+
         <input
           className="btn btn-block"
           type="submit"
           value="Create Assignment"
         />
       </form>
-      {/*  
-      <br />
-      <br />
-      <br />
-      <h2 className="text-3xl mb-8">All Products</h2>
-
-      <div className="overflow-x-auto">
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Product</th>
-              <th>Description</th>
-              <th>Price</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {loadProducts.map((product) => (
-              <>
-                <tr>
-                  <td>
-                    <div className="flex items-center space-x-3">
-                      <div className="avatar">
-                        <div className="mask mask-squircle w-12 h-12">
-                          <img src={product.photo} />
-                        </div>
-                      </div>
-                      <div>
-                        <div className="font-bold">{product.name}</div>
-                        <div className="text-sm opacity-50">
-                          {product.brandName}
-                        </div>
-                      </div>
-                    </div>
-                  </td>
-                  <td>
-                    {product.sortDescription}
-                    <br />
-                    <span className="badge badge-ghost badge-sm">
-                      {product.productType}
-                    </span>
-                  </td>
-                  <td>{product.price}</td>
-
-                  <th>
-                    <button className="btn btn-ghost btn-xs">Edit</button>
-                    <button className="btn btn-danger btn-xs">Delete</button>
-                  </th>
-                </tr>
-              </>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      */}
     </div>
   );
 };
